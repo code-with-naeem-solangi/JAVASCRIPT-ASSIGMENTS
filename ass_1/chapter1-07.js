0162e9e9ac55e796fe329644f5124b2cfb084e51
M NAEEM SOLANGI <137903743+code-with-naeem-solangi@users.noreply.github.com>
2023-07-03 12:10:18 +0500

    assigment-1

---
+++ ass_1/chapter1-07.js
@@ -0,0 +1,659 @@
+
+
+// // CHAPTER ONE
+
+
+
+// // Q ONE
+
+
+alert ( "WELLCOME TO THE MY ALERT FOR JAVASCRIPT")
+
+
+// // Q TWO
+
+
+alert ("Error!Please enter a valid password" )
+
+
+
+// // Q THREE
+
+alert ( "Welcome to JS Land.....\nHappy Coding!")
+
+
+
+
+// // Q FOUR
+
+alert ("Welcome to JS Land")
+
+alert ("Happy Coding!\n Prevent this page from creating additional dialoges.")
+
+
+
+
+// // Q FIVE
+
+alert ("Heloo...I can run JS through my web browser's console")
+
+console.log ("Heloo...I can run JS through my web browser's console")
+
+
+
+// // Q SIX
+
+document.write("CHAPTER ONE Q SIX<h1>Welcome To The JS Website</h1>")
+
+
+// // CHAPTER ONE COMPLETE
+
+
+
+// // CHAPTER TWO
+
+
+// // Q ONE
+
+var userName = " Ahmad ali"
+
+alert( userName)
+
+
+
+
+// // Q TWO
+
+var myName = " Muhammad Ali Solangi"
+
+alert (myName)
+
+
+
+
+// // Q THREE
+
+var message =" Hello World"
+
+alert(  message )
+
+
+
+
+// // Q FOUR
+
+var userName = "Ali Khan"
+
+var num = 17;
+
+var age = " Years old"
+
+var message = "Prevent this page from creating additional dialogs "
+
+var work = " SSUET"
+
+var partTime = "Mobile & Cloud Computing - Module A"
+
+alert(" Ali Khan\n 17 years old\n Prevent this page from creating additional dialogs \n SSUET \n Mobile & Cloud Computing - Module A\n Prevent this page from creating additional dialogs")
+
+
+
+
+// // Q FIVE
+
+var userName = "PIZZA"
+var userName2 = "PIZZ"
+var userName3 = "PIZ"
+var userName4 = "PI"
+var userName5 = "P"
+
+alert( "PIZZA\nPIZZ\nPIZ\nPI\nP")
+
+
+
+
+// // Q  SIX
+
+var intro=" I Am Software Developer In"
+
+var country=" PAKISTAN,"
+
+var inlive= " married to ABC "
+
+var num=   2;
+
+var chaid=" Kids"
+
+alert ( intro + country + inlive + num + chaid)
+
+
+
+
+// // Q SEVEN
+
+var notes= " My email address is"
+
+var email=" career@incitertech.com"
+
+alert ( notes + email)
+
+
+
+
+// // Q EIGHT
+
+var study = " I am trying to learn froom the book"
+
+var book="  “A smarter way to learn JavaScript”"
+
+alert ( study + book)
+
+
+
+// // Q NINE
+
+var userName = " My name is Ali Khan"
+
+var intro1 = " I am "
+
+var num =   17;
+
+var intro2 = " years old"
+
+var study = "  I study in SSUET"
+
+var study2 = " I take Mobile & Cloud Computing, Module A classes"
+
+alert( " My name is Ali Khan\n I am 17 years old \n I study in SSUET \n I take Mobile & Cloud Computing, Module A classes")
+
+
+
+
+// // Q TEN
+
+var backRound = "Yah! I can write HTML content through"
+
+var lang= " Javascript"
+
+document.write (backRound + lang)
+
+
+
+// // CHAPTER TWO COMPLETE
+
+
+
+
+
+// // CHAPTER THREE START
+
+
+// // Q ONE
+
+var num = 14;
+
+
+alert ("I am " + num + " years old")
+
+
+
+// // Q TWO
+
+var masage = "You Have visited this site "
+
+var num = 30 ;
+
+var masage2 =" times"
+
+alert ( masage + num + masage2)
+
+
+
+
+// // Q THREE
+
+var massege = " My birth year is"
+
+var num = 1990;
+
+var massege2 = " Data type of my declared variable is number"
+
+document.write (" My birth year is 1990 <br> Data type of my declared variable is number")
+
+
+
+
+// // Q FOUR
+
+
+var myName = " Ahmad Ali"
+
+var order = " ordered "
+
+var num = 5;
+
+var sport = " cricket bat(s)"
+
+var store = " on peshawer store"
+
+document.write (  myName + order + num + sport + store )
+
+// // CHAPTER THREE COMPLETE
+
+
+
+
+
+// // CHAPTER FOUR  START
+
+// // Q ONE
+
+var massage = "Ahmad ali "
+
+var myName = "Ali khan"
+
+var my_name = " Kashif Meman"
+
+alert ("Ahmad Ali\nAli Khan\nKashif Meman")
+
+
+// // Q TWO IN HTML
+
+
+
+// // Q THREE IN HTML
+
+// // Chapter four complete
+
+
+
+
+
+
+// //chapter five six seven start
+
+
+
+// // Q one
+
+var num = 13;
+
+var anotherNum = 9;
+
+var conc = num + anotherNum
+
+alert(" sum of " + num + " and " + anotherNum + " is " + conc)
+
+
+
+
+
+
+// // Q two
+
+
+var num = 9;
+
+var anotherNum = 13;
+
+var conc = anotherNum - num
+
+alert("  result of subtracting " + num + " from " + anotherNum + " is " + conc)
+
+var num = 13;
+
+var anotherNum = 9;
+
+var newNum = (13 * 9)
+
+alert(" Result of multiplying " + num + " and " + anotherNum + " is " + newNum)
+
+
+var num = 13;
+
+var anotherNum = 9;
+
+var newNum = (num / anotherNum)
+
+alert(" Result of dividing " + num + " and " + anotherNum + " is " + newNum)
+
+
+var num = 13;
+
+var anotherNum = 9;
+
+var newNum = (num % anotherNum)
+
+alert(" Remainder of dividing " + num + " and " + anotherNum + " is " + newNum)
+
+
+
+
+// // Q three
+
+var masage= "Value after variable declaration is: undefined"
+
+document.write ( masage)
+
+
+var num = 3;
+
+document.write ("Initial value:" + num )
+
+
+
+var anotherNum = 3;
+var newNumbr = ++anotherNum
+
+document .write ("Value after increment is:" + newNumbr)
+
+var myNum= 7;
+var newNumbr = ++myNum
+
+document.write ("Value after addition is: "+ newNumbr)
+
+
+var popularNum = 11;
+
+var newNum= --popularNum
+
+
+document.write ("Value after decrement is:" + newNum)
+
+var lowNum = 3;
+
+var dividing=3%2
+
+document.write ("The remainder is:" + dividing )
+
+
+
+
+
+// // Q four
+
+ var num= 600;
+
+ var anotherNum = 5;
+
+ var newNum = (600 * 5)
+
+ document.write ("Total cost to buy " + anotherNum +" tickets to a movie is " + newNum +" PKR")
+
+
+
+
+
+//  // // Q FIVE
+
+var num = 4 ;
+var num2= 1 ;
+var newNum=4*1;
+
+document.write(num+" x "+num2+" = "+newNum)
+
+var num = 4 ;
+var num2= 2 ;
+var newNum=4*2;
+
+document.write(num+" x "+num2+" = "+newNum )
+
+var num = 4 ;
+var num2= 3 ;
+var newNum=4*3;
+
+document.write(num+" x "+num2+" = "+newNum )
+
+var num = 4 ;
+var num2= 4 ;
+var newNum=4*4;
+
+document.write(num+" x "+num2+" = "+newNum )
+
+var num = 4 ;
+var num2= 5 ;
+var newNum=4*5;
+
+document.write(num+" x "+num2+" = "+newNum )
+var num = 4 ;
+var num2= 6 ;
+var newNum=4*6;
+
+document.write(num+" x "+num2+" = "+newNum )
+var num = 4 ;
+var num2= 7 ;
+var newNum=4*7;
+
+document.write(num+" x "+num2+" = "+newNum )
+
+var num = 4 ;
+var num2= 8 ;
+var newNum=4*8;
+
+document.write(num+" x "+num2+" = "+newNum )
+var num = 4 ;
+var num2= 9 ;
+var newNum=4*9;
+
+document.write(num+" x "+num2+" = "+newNum )
+
+var num = 4 ;
+var num2= 10 ;
+var newNum=4*10;
+
+document.write(num+" x "+num2+" = "+newNum )
+
+
+
+
+
+// // // Q Six
+
+
+
+
+var c = 12
+
+var f = (c*9/5) + 32
+
+
+document.write(c+"&deg;C"+" is "+f+"&deg;F")
+
+var c= 68 
+var f = (f-32) * 5/9
+
+document.write(c+"&deg;F"+" is "+ f+"&deg;C")
+
+
+
+
+
+// // Q Seven
+
+var iOne_price=560
+var iOne_totl=2
+var iTwo_price= 200
+var iTwo_totl=5
+
+var Result_iOne=560*2
+var Result_iTwo= 200*5
+var shiping_fee= 150
+
+var fainl_result = Result_iOne+Result_iTwo+shiping_fee
+
+document.write("Price of item 1 is "+iOne_price+"<br>"+" Quantity of item 1 is "+iOne_totl+"<br>")
+
+document.write("Price of item 2 is "+ iTwo_price+"<br>"+" Quantity of item 2 is "+iTwo_totl+"<br>")
+
+document.write("Shipping charges " +shiping_fee+"<br>")
+
+document.write("Total cost of your order is "+ fainl_result + " PKR")
+
+
+
+
+
+// // Q Eight
+
+var mediumNumb= 200;
+var anotherNum= 300;
+var popularNum= 400;
+
+var obtained = mediumNumb+anotherNum+popularNum;
+
+var total = 1200;
+
+var percentage = (obtained / total) * 100;
+
+document.write("Total marks :"+total+"<br> Marks obtained :"+obtained+"<br>percentage :"+percentage+"%")
+
+
+
+
+
+// //  Q nine
+
+var massege ="Total currency in PKR :"
+
+var usDoolar= 104
+var riyal=28
+
+var newNum= 104*10 + 28*25
+
+document.write(massege+newNum)
+
+
+
+
+// // Q ten
+
+var num = 5;
+
+var anotherNum = 10;
+
+var lowNum = 2;
+
+var newNum= 5*10/2
+
+alert(newNum)
+
+
+
+
+// // Q Elevan
+
+var massege="Current Year : "
+var num= 2015;
+var massege2 = "Birth Year : "
+var num2 = 1992;
+var massege3 = "They are either "
+var divide = num - num2 
+var divide2 = num - --num2
+
+document.write(massege + num +"<br>"+ massege2 + num2 +"<br><br>"+ massege3 + divide +" or " +divide2+ " Years old")
+
+
+
+// Q twell
+
+
+
+var radius = 10;
+var pieValue = 3.14;
+
+var circumference=  2*pieValue*radius
+var area= pieValue*(radius*radius)
+
+document.write(
+    
+    "The Geometrizer"+"<br><br>"+
+
+    "Radius of a circle : "+ radius +"<br>"+
+     "The Circumference is : "+ circumference +"<br>"+
+
+     "The Area is : " + area
+
+
+)
+
+
+
+
+
+// // Q Tharteen
+
+var massege= "Result: "
+var massege2=" The value of a is : "
+var num = 10 
+
+document.write(massege+"<br>"+massege2+num+"<br>.....................................................................<br><br>")
+
+var num= 10
+var massege=" The value of ++newNum a is: "
+var newNum= ++num
+var massege2= " Now the value of newNum is:"
+
+document.write(massege+ newNum+"<br>"+massege2+newNum+"<br><br>")
+
+
+var num= 10
+var massege=" The value of newNum++  is: "
+var newNum= num ++
+var mustNum= num++
+var massege2= " Now the value of newNum is:"
+
+document.write(massege+ newNum+"<br>"+massege2+mustNum+"<br><br>")
+
+var num= 10
+var massege=" The value of --newNum  is: "
+var newNum= --num
+
+var massege2= " Now the value of newNum is:"
+
+document.write(massege+ newNum+"<br>"+massege2+ newNum + "<br><br>")
+
+var num= 10
+var massege=" The value of newNum-- is: "
+var newNum= num--
+var popl= num--
+
+var massege2= " Now the value of newNum is:"
+
+document.write(massege+ newNum+"<br>"+massege2+ popl + "<br><br>")
+
+
+
+
+// Q Fourteen
+
+var a=2
+var Result=--a
+
+var a= 2, b=1;
+var Result2= --a - --b;
+
+var a= 2, b=1;
+var Result3= --a - --b + ++b;
+
+var a= 2, b=1;
+var Result4 = --a - --b + ++b  + b--;
+
+document.write(Result+" <br> "+Result2+" <br> "+Result3+"<br>"+Result4)
+
+
+
+
+//  Q  Fifteen
+
+var snck= "RIO"
+var age= 22;
+var maxim_age=65
+var amunt_day=25
+
+var Result= 65 - 22
+var Result2= 43*25
+
+document.write("You will need " + Result2 +" to last you until the ripe old age of " + Result)
+
+
+
